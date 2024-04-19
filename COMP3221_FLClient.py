f6c7b7d72b5eca56e513160d93b22066b8d9411b
Connor Smith <71867196+ConnorSapphire@users.noreply.github.com>
2024-04-19 22:35:05 +1000

    Documented code for client

--- COMP3221_FLClient.py
+++ COMP3221_FLClient.py
@@ -33,7 +33,12 @@ class Client:
         self.confirmed = False
         self.iteration = 0
         
-    def start(self):
+    def start(self) -> None:
+        """
+        Start all processes for client instance. Including retreiving data, creating log
+        files, creating the listening socket thread, and sending the handshake message to the
+        server.
+        """
         print(f"I am client {self.client_id.strip("client")}")
         self.create_log()
         self.retrieve_data()
@@ -41,26 +46,33 @@ class Client:
         self.listener_thread.start()
         self.send_message(f"CONNECTION ESTABLISHED")
 
-    def stop(self):
+    def stop(self) -> None:
+        """Stop the listener thread.
+        """
         try:
             self.stop_event.set()
             self.listener_thread.join()
         except Exception:
             pass
 
-    def listen_to_server(self):  # Listen on port 6001, 6002, etc.
+    def listen_to_server(self) -> None:  # Listen on port 6001, 6002, etc.
+        """
+        Listen to the server for incoming messages across the socket using the 
+        client's port.
+        """
         print(f"Client listening on port {self.port}")
-        while True:
+        while not self.stop_event.is_set():
             try:
                 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
                     client_socket.bind((HOST, self.port))
                     client_socket.listen(5)
-                    
                     conn, addr = client_socket.accept()
                     with conn:
                         try:
+                            # first bit represents type of message
                             data = conn.recv(1)
-                            if data == b"0":
+                            # check if expecting a model
+                            if data == b"0": # expecting model
                                 data = b""
                                 while True:
                                     packet = conn.recv(1048)
@@ -80,8 +92,8 @@ class Client:
                                         update.start()
                                     except Exception as e:
                                         print(f"Failed: {e}")
-                                
-                            elif data == b"1":
+                            # check if confirming a model was received by the server
+                            elif data == b"1": # confirming model received
                                 self.confirmed = True
                         except Exception as e:
                             print("Could not read from server: {e}")
@@ -89,7 +101,15 @@ class Client:
             except Exception as e:
                 print(f"Can't connect to the listener socket: {e}")
 
-    def send_message(self, message):
+    def send_message(self, message: str) -> None:
+        """
+        Send a message to the server. Used for sending the handshake method to tell the server
+        the client is available.
+
+        Args:
+            message (str): String message to be sent to the server.
+        """
+        # define message
         message = {
             "client_id": self.client_id,
             "port": self.port,
@@ -97,30 +117,42 @@ class Client:
             "content": message,
         }
 
+        # attempt to send message
         try:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
                 client_socket.connect((HOST, SERVER_PORT))
-                client_socket.sendall(b"0")
+                # send binary 0 first to inform server to expect message in this format
+                client_socket.sendall(b"0") 
+                # send message
                 client_socket.sendall(json.dumps(message).encode())
                 print(f"Message sent to server: {message["content"]}")
                 client_socket.close()
         except Exception as e:
             print(f"Message failed: {e}")
             
-    def send_model(self):
+    def send_model(self) -> None:
+        """
+        Send the model back to the server. Should be called after the model has been trained.
+        """
+        # define the message
         sent = False
         message = {
             "client_id": self.client_id,
             "iteration": self.iteration,
             "model": self.model,
         }
+        
+        # attempt to send the message
         while not sent:
             try:
                 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
                     client_socket.connect((HOST, SERVER_PORT))
+                    # send binary 1 first to inform server to expect message in this format
                     client_socket.sendall(b"1")
+                    # send the message
                     client_socket.sendall(pickle.dumps(message))
-                    time.sleep(0.1)
+                    # wait and then confirm whether message was received
+                    time.sleep(0.15)
                     sent = self.confirmed
                     self.confirmed = False
                     client_socket.close()
@@ -128,14 +160,25 @@ class Client:
                 print(f"Model failed to send: {e}")
         print("\tModel sent to server")
     
-    def evaluate(self):
+    def evaluate(self) -> torch.Tensor:
+        """
+        Test the model against the testing data using Mean Squared Error (MSE).
+
+        Returns:
+            torch.Tensor: The MSE error of the current model on the testing data.
+        """
         pred = self.model(self.X_test)
         loss = self.loss_fn(pred, self.Y_test)
         print(f"\tTesting MSE: {loss:.04f}")
         self.write_log(f"\tTesting MSE: {loss:.04f}")
         return loss
     
-    def update(self):
+    def update(self) -> None:
+        """
+        Updates the global model using the client instances specific update method.
+        Either gradient descent or mini-batch gradient descent. Sends the model back
+        to the server once the training is complete.
+        """
         self.evaluate()
         print(f"\tUpdating local model:")
         if self.opt_method == 0:
@@ -144,9 +187,10 @@ class Client:
             self.mini_batch()
         self.send_model()
     
-    def gradient_descent(self):
-        """Performs the gradient descent algorithm on the current model.
-        This multiple times, as determined by self.epochs.
+    def gradient_descent(self) -> None:
+        """
+        Performs the gradient descent algorithm on the current model.
+        This occurs multiple times, as determined by self.epochs.
         The MSE result before and after all epochs are printed to the terminal and
         saved in the logs.
         """
@@ -164,12 +208,19 @@ class Client:
         self.write_log(f"\tPost-update training MSE: {losses[-1]:.04f}")
     
     def mini_batch(self):
+        """
+        Performs the mini-batch gradient descent algorithm on the current model.
+        This occurs multiple times, as determined by self.epochs.
+        The MSE result before and after all epochs are printed to the terminal and
+        saved in the logs.
+        """
         pass
 
     def retrieve_data(self):
-        """Retrieves the training data and testing data for the client from the files.
-        This data is stored inside the client instance and used for evaluating and training
-        the model in each iteration.
+        """
+        Retrieves the training data and testing data for the client from the files.
+        This data is converted into Tensors and stored inside the client instance then
+        used for evaluating and training the model in each iteration.
         """
         # retrieve training data
         df = pd.read_csv(f"./FLData/calhousing_train_{self.client_id}.csv")
@@ -187,6 +238,12 @@ class Client:
         print("Data retrieved from files")
         
     def create_log(self) -> None:
+        """
+        Creates and clears the log file for this specific instance of the client. The log file
+        is stored at:
+        ./FLLogs/<client_id>_log.txt
+        for each client.
+        """
         try:
             with open(f"./FLLogs/{self.client_id}_log.txt", "w") as f:
                 f.close()
@@ -194,6 +251,12 @@ class Client:
             print(f"Error creating log file: {e}")
         
     def write_log(self, message: str) -> None:
+        """
+        Appends a message to the log file for this specific instance of the client, includes a
+        new line. The log file is stored at:
+        ./FLLogs/<client_id>_log.txt
+        for each client.
+        """
         try:
             with open(f"./FLLogs/{self.client_id}_log.txt", "a") as f:
                 f.write(message + "\n")
